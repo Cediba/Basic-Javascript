c28375edf38d679296b56db70a41154535c4e4f6
Cedric <cedric.balmer@gmx.ch>
2019-10-02 11:28:06 -0700

    added next problem

--- Javascript-basics/basics.js
+++ Javascript-basics/basics.js
@@ -16,3 +16,19 @@ function getAreaOfTriangle(side1, side2, side3){
 }
 
 document.getElementById("demo1").innerHTML = (getAreaOfTriangle(5,6,7));
+
+
+/*** program to rotate the string */
+
+function animate_string(id) 
+{
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0]; // assuming no other children
+    var text = textNode.data;
+
+setInterval(function () 
+{
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}
