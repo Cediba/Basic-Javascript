c99bdd85c1884659e09f9c48aa0f70b13be16844
Cedric <cedric.balmer@gmx.ch>
2019-10-02 09:41:02 -0700

    function to calculate area of triangle

--- Javascript-basics/basics.js
+++ Javascript-basics/basics.js
@@ -5,3 +5,14 @@ var d = new Date();
 document.getElementById("demo").innerHTML = d;
 
 console.log(d);
+
+
+/*** area of a triangle */
+
+function getAreaOfTriangle(side1, side2, side3){
+    var p = (side1+side2+side3)/2;
+    var a = Math.sqrt(p*(p-side1)*(p-side2)*(p-side3));
+    return a;
+}
+
+document.getElementById("demo1").innerHTML = (getAreaOfTriangle(5,6,7));
